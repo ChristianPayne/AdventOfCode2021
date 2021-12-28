4c6f19cec83ea74c0f01932526e8c9ab8ead3b51
Christian Payne <christianpayne522@gmail.com>
2021-12-28 14:57:52 -0800

    Finished Day 3 part 2!

--- 3/index.dart
+++ 3/index.dart
@@ -5,13 +5,42 @@ void main(List<String> args) {
   List<String> data =
       LineSplitter().convert(new File('./input.txt').readAsStringSync());
 
+  // Challenge1(data);
+  Challenge2(data);
+}
+
+CountData FindCountOfIndex(int index, List data) {
+  int zero = 0;
+  int one = 0;
+
+  for (int i = 0; i < data.length; i++) {
+    if (data[i][index] == "0") {
+      zero++;
+    } else {
+      one++;
+    }
+  }
+  // print("Zero: $zero One: $one");
+  return new CountData()
+    ..mostCommon = "${zero > one ? "0" : "1"}"
+    ..ones = one
+    ..zeros = zero;
+}
+
+class CountData {
+  int ones = 0;
+  int zeros = 0;
+  String mostCommon = "";
+}
+
+void Challenge1(data) {
   var gammaRateBinary = "";
   var epsilonRateBinary = "";
 
   for (int i = 0; i < 12; i++) {
     var result = FindCountOfIndex(i, data);
-    gammaRateBinary += result.gammaRateOutput;
-    epsilonRateBinary += result.epsilonRateOutput;
+    gammaRateBinary += result.mostCommon == "0" ? "0" : "1";
+    epsilonRateBinary += result.mostCommon == "1" ? "0" : "1";
   }
   var gammaRateNumber = int.parse(gammaRateBinary, radix: 2);
   var epsilonRateNumber = int.parse(epsilonRateBinary, radix: 2);
@@ -26,31 +55,68 @@ void main(List<String> args) {
       "Final number for calculation is ${gammaRateNumber * epsilonRateNumber}");
 }
 
-class Count {
-  int zeroCount = 0;
-  int oneCount = 0;
-  String gammaRateOutput = "";
-  String epsilonRateOutput = "";
-}
-
-Count FindCountOfIndex(int index, List data) {
-  int zero = 0;
-  int one = 0;
-
-  for (int i = 0; i < data.length; i++) {
-    if (data[i][index] == "0") {
-      zero++;
-    } else {
-      one++;
-    }
+void Challenge2(data) {
+  List<String> RemoveDataInList(
+      String checkString, int index, List<String> data) {
+    data.removeWhere((element) => element[index] == checkString);
+    return data;
   }
 
-  var gammaRateOutput = zero > one ? "0" : "1";
-  var epsilonRateOutput = zero > one ? "1" : "0";
+  int CheckListLength(List list) {
+    // print("List length: ${list.length}");
+    return list.length;
+  }
 
-  return new Count()
-    ..zeroCount = zero
-    ..oneCount = one
-    ..gammaRateOutput = gammaRateOutput
-    ..epsilonRateOutput = epsilonRateOutput;
+  var oxygenGenRatingBinary = "";
+  var cO2ScrRatingBinary = "";
+
+  // Clone Data
+  List<String> oxygenData = List.from(data);
+
+  // oxygenGenRatingBinary
+  var binaryTracker = "";
+  for (int i = 0; i < 12; i++) {
+    if (CheckListLength(oxygenData) == 1) break;
+    var result = FindCountOfIndex(i, oxygenData);
+    if (result.zeros > result.ones) {
+      binaryTracker += "0";
+    } else if (result.ones > result.zeros) {
+      binaryTracker += "1";
+    } else {
+      binaryTracker += "1";
+    }
+    oxygenData = RemoveDataInList(binaryTracker[i], i, oxygenData);
+  }
+  oxygenGenRatingBinary = oxygenData[0];
+
+  // Reset Data
+  oxygenData = List.from(data);
+  binaryTracker = "";
+
+  // cO2ScrRatingBinary
+  for (int i = 0; i < 12; i++) {
+    if (CheckListLength(oxygenData) == 1) break;
+    var result = FindCountOfIndex(i, oxygenData);
+    if (result.zeros < result.ones) {
+      binaryTracker += "0";
+    } else if (result.ones < result.zeros) {
+      binaryTracker += "1";
+    } else {
+      binaryTracker += "0";
+    }
+    oxygenData = RemoveDataInList(binaryTracker[i], i, oxygenData);
+  }
+  cO2ScrRatingBinary = oxygenData[0];
+
+  var oxygenGenRatingNumber = int.parse(oxygenGenRatingBinary, radix: 2);
+  var cO2ScrRatingNumber = int.parse(cO2ScrRatingBinary, radix: 2);
+
+  print("oxygenGenRatingBinary: $oxygenGenRatingBinary");
+  print("CO2ScrRatingBinary: $cO2ScrRatingBinary");
+
+  print("oxygenGenRatingNumber: $oxygenGenRatingNumber");
+  print("CO2ScrRatingNumber: $cO2ScrRatingNumber");
+
+  print(
+      "Final number for calculation is ${oxygenGenRatingNumber * cO2ScrRatingNumber}");
 }
