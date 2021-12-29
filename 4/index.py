210dff3aa0474337d01c924c3a4bb71e9dd5f4d7
Christian Payne <christianpayne522@gmail.com>
2021-12-29 11:55:51 -0800

    Day 4 code cleanup.

--- 4/index.py
+++ 4/index.py
@@ -1,7 +1,5 @@
 import csv
-from typing import List, final
-# print(boards)
-
+from typing import List
 class Number:
   value = 0
   marked = False
@@ -11,19 +9,16 @@ class Number:
 
   def mark (self):
     self.marked = True
-    # print("I got a mark!")
 
 class Row:
   numbers = []
   
   def __init__(self, numbers):
       self.numbers = [Number(num) for num in numbers]
-      # print([num.value for num in self.numbers])
 
   # If the number we pass matches in the row, change that num to marked = True
   def markNumber (self, number):
     for num in self.numbers:
-      # print(f"Checking {num.value} against {number}")
       if(num.value == number):
         num.mark()
         
@@ -54,14 +49,12 @@ class Board:
     wins = [row.winCheck() for row in self.rows if row.winCheck() == True]
     if (len(wins) >= 1):
       self.won = True
-      # print("Won with a row!")
     
     # We also need to do a column check here.
     for i in range(len(self.rows[0].numbers)):
       marks = [row.numbers[i] for row in self.rows if row.numbers[i].marked == True]
       if(len(marks) == len(self.rows)):
         self.won = True
-        # print("Won with a column!")
 
   def sumUnmarked (self) -> int:
     return sum([row.sumUnmarked() for row in self.rows])
@@ -74,8 +67,7 @@ with open('./input.txt') as csv_file:
   csv = csv.reader(csv_file, delimiter=' ')
   rawNumbers = next(csv)[0]
   numbers = [int(number) for number in rawNumbers.split(',')]
-  boardData = [
-    [int(num) for num in board if len(num) > 0] 
+  boardData = [[int(num) for num in board if len(num) > 0] 
     for board in csv if len(board) != 0]
   
   boardQueue = []
@@ -85,7 +77,6 @@ with open('./input.txt') as csv_file:
       newBoard = Board(boardQueue)
       boards.append(newBoard)
       boardQueue = []
-      # print(f"New Board {newBoard}")
     else:
       boardQueue.append(row)
 
@@ -96,31 +87,16 @@ def Play ():
   for number in numbers:
     if(len(winningBoards) == len(boards)):
       break
-    else:
-      print(f"{len(winningBoards)} | {len(boards)}")
 
     print(f"New Number {number}")
     for board in [board for board in boards if board.won == False]:
       board.markNumber(number)
       board.winCheck()
       if(board.won):
-        # print(f"Board Won!")
         winningBoards.append(board)
         finalNumber = number
-        # print(finalNumber)
-  # print(winningBoards)
   print(f"Last winning board {winningBoards[-1].name} with number {finalNumber}")
-  print([[num.marked for num in row.numbers] for row in winningBoards[-1].rows])
   print(f"Final Score: {winningBoards[-1].sumUnmarked() * finalNumber}")
+
+
 Play()
-
-
-
-# board1 = Board([
-#   [1,2,123],
-#   [1,2,789],
-#   [12,12,12],
-# ])
-
-# board1.markNumber(12)
-# print(board1.winCheck())
